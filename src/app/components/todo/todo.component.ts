c51ceb8289a6e09df0806a98c96a7029f2a86e06
sumalsurendra1999@gmail.com <sumalsurendra1999@gmail.com>
2025-02-14 01:24:22 +0530

    Added the components

---
+++ src/app/components/todo/todo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  imports: [],
+  templateUrl: './todo.component.html',
+})
+export class TodoComponent {
+  
+}
